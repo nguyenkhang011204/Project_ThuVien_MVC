157e22d1f51ad12d3600627cbf0a8bc139280c45
nguyenkhang011204 <nguyenkhang011204@gmail.com>
2026-07-12 00:39:39 +0700

    [TV13.1]: Download bootstrap

---
+++ app/views/Layouts/master.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        <?= $title ?? "Thư viện"; ?>
+    </title>
+</head>
+
+<body>
+
+    <?php require "header.php"; ?>
+
+    <div class="container">
+
+        <?php require "sidebar.php"; ?>
+
+        <main>
+
+        </main>
+
+    </div>
+
+    <?php require "footer.php"; ?>
+
+</body>
+
+</html>
